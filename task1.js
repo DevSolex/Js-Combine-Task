01d7d472bf2c5870a81850dd743653475ef1715c
DevSolex <devsolex6@gmail.com>
2026-03-23 15:00:27 +0100

    initial commit

---
+++ task1.js
@@ -0,0 +1,43 @@
+/*
+Iredia039:
+
+Exercise 1 — Quote Printer
+
+Store a quote and an author name in two separate variables, then print them in this format:
+
+Expected Output
+"Your quote here" - Author Name
+
+*/
+
+
+
+/* 
+ValJnr-dev1:
+
+Exercise 2 — Biography Builder
+
+Create four variables: firstName, lastName, country, and job. Use them to print a sentence in this format:
+
+Expected Output
+Andrew is a developer from India.
+
+*/
+
+
+/*
+Amaka And Solex:
+Exercise 3 — Profile Card Generator
+
+Create variables for name, age, country, and hobby, then display a formatted profile card like this:
+
+Expected Output
+====================
+Name : Solex
+Age : 34
+Country : Nigeria
+Hobby : Coding
+====================
+
+*/
+
